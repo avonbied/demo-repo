e56634968d0cff54a054ed491ad68b467cb794a6
Lex <git@avonbied.me>
2025-02-12 04:49:48 +0000

    Update code

--- self-service-iac/common/main.tf
+++ self-service-iac/common/main.tf
@@ -2,19 +2,3 @@ resource "azurerm_resource_group" "example" {
 	name		= "rg-iacdemo"
 	location	= "eastus"
 }
-
-resource "azurerm_virtual_network" "example" {
-	resource_group_name	= azurerm_resource_group.example.name
-	location			= azurerm_resource_group.example.location
-
-	name				= "vnet-iacdemo"
-	address_space		= ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "example" {
-	resource_group_name		= azurerm_resource_group.example.name
-	virtual_network_name	= azurerm_virtual_network.example.name
-
-	name					= "subnet-iacdemo"
-	address_prefixes		= ["10.0.0.0/24"]
-}
--- self-service-iac/template-usage/terraform.tf
+++ self-service-iac/template-usage/terraform.tf
@@ -16,14 +16,3 @@ provider "azurerm" {
 data "azurerm_resource_group" "example" {
 	name = "rg-iacdemo"
 }
-
-data "azurerm_virtual_network" "example" {
-	name				= "vnet-iacdemo"
-	resource_group_name	= data.azurerm_resource_group.example.name
-}
-
-data "azurerm_subnet" "example" {
-	name					= "subnet-iacdemo"
-	resource_group_name		= data.azurerm_resource_group.example.name
-	virtual_network_name	= data.azurerm_virtual_network.example.name
-}
